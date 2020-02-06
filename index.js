347ff795b7e5e7e45901285926a5abb204c354e9
BERTRAND ZANCO <bertrand.zanco@adeo.com>
2020-02-06 11:44:49 +0100

    Update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 const fs = require("fs");
 
 try {
+    console.log("Load event file: "+process.env.GITHUB_EVENT_PATH);
     var eventContent = fs.readFileSync(process.env.GITHUB_EVENT_PATH);
     var eventJsonContent = JSON.parse(eventContent);
     var issueContent = JSON.parse(eventJsonContent.issue.body);
